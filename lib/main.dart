1c15522e758d0618263e426e86237efc1d808d63
RyoNakahara <ryo072144@gmail.com>
2023-03-20 22:38:44 +0900

    answer

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Exercise 1',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+
+        //問１： ここでは基本色しか指定できません。
+        primarySwatch: Colors.orange,
       ),
       home: const MyHomePage(),
     );
@@ -25,15 +27,27 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Color(8桁のカラーコード)で色を自由に指定することができます。最初の2桁は透過率を表します。00で完全に透明になります。
         appBar: AppBar(title: const Text('タイトル', style: TextStyle(fontSize: 20, color: Color(0xff222222), fontWeight: FontWeight.bold),),),
+
+        //問２： 真黒(0xff000000)や真白(0xffffffff)は目が疲れるので、あまり使わないようにしましょう。
+        //Flutterのカラーコードは8桁ですが、頭の2桁は透過率を表します。
+        backgroundColor: const Color(0xffeeeeee),
         body: Column(
           children: [
             Container(
               padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
               child: Row(
+
+                //問７： mainAxisAlignmentでColumnやRowと平行に位置を調節できます。
+                //crossAxisAlignmentはColumnやRowと直角に位置を調整できます。
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: const [
                   Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
+
+                  //問４: アイコンの一覧はこちらで確認できます(https://api.flutter.dev/flutter/material/Icons-class.html)。
+                  //SizedBoxはちょっとした隙間を作りたいときに便利です。
+                  Icon(Icons.thumb_down, color: Colors.red, size: 30,),
                   SizedBox(width: 10,),
                   Text('タイル１', style: TextStyle(fontSize: 18, color: Color(0xff222222),),)
                 ],
@@ -41,8 +55,11 @@ class MyHomePage extends StatelessWidget {
             ),
             const Divider(height: 20,),
             Container(
-              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+
+              //問５： paddingはWidgetの大きさを広げるプロパティです。
+              padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
                   SizedBox(width: 10,),
@@ -54,10 +71,26 @@ class MyHomePage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: const [
                   Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
                   SizedBox(width: 10,),
-                  Text('タイル３', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
+
+                  //問６： fontWeightで文字の太さを調整できます。
+                  Text('タイル３', style: TextStyle(fontSize: 18, color: Color(0xff222222), fontWeight: FontWeight.bold))
+                ],
+              ),
+            ),
+            const Divider(height: 20,),
+
+            //問３： Dividerで分けた部分をタイルとしています。
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+              child: Row(
+                children: const [
+                  Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
+                  Text('タイル４', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
                 ],
               ),
             ),
